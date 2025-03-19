48fdf11164f50d3e4c58d61861e09df86da62fb3
Saul De la fuente <saul.delafuenteb@gmail.com>
2025-03-19 10:04:28 -0600

    Se termina de desarrolar la parte de conexion abase de datos de MongoDB junto con socket.io

--- services/collections.service.js
+++ services/collections.service.js
@@ -1,12 +1,21 @@
+import { ObjectId } from 'mongodb'
 import { db } from './../db/mongoClient.js'
 import Boom from '@hapi/boom'
 
-export default class Collection{
+class Collection{
   constructor(){
 
   }
   async create(collection,data){
-
+    try {
+      const result = await db.collection(collection).insertOne(data)
+      return result
+    } catch (error) {
+      if(Boom.isBoom(error)){
+        throw error
+      }else{
+      throw Boom.badImplementation('Algo falto aqui para subir',error)}
+    }
   }
 
   async getAll(collection){
@@ -29,12 +38,100 @@ export default class Collection{
     }
   }
 
-  async getOneById(collection,id){
+  async addFIles(collection,files,data){
+    try {
 
+      const newData = {
+        ...data,files
+      }
+      const result = await db.collection(collection).insertOne(newData)
+      return result
+
+    } catch (error) {
+      if(Boom.isBoom(error)){
+        throw error
+      }else{
+      throw Boom.badImplementation('Algo falto aqui para subir',error)}
+    }
   }
 
-  async updateOneById(collection,id){}
+  async getOneById(collection,id){
+    try {
 
-  async deleteOneById(collection,id){}
+      if (!ObjectId.isValid(id)) {
+        throw Boom.badRequest('El ID proporcionado no es válido')
+      }
+
+      const result = await db.collection(collection).findOne({ _id: new ObjectId(id) })
+
+      if (!result) {
+        throw Boom.notFound(`No se encontró un documento con ID ${id} en la colección ${collection}`)
+      }
+
+      return result
+    } catch (error) {
+      if(Boom.isBoom(error)){
+        throw error
+      }else{
+      throw Boom.badImplementation('Sucito un error al obtener la información')
+      }
+    }
+  }
+
+  async updateOneById(collection,id,newData){
+    try {
+
+      if(!ObjectId.isValid(id)){
+        throw Boom.badRequest('El ID proporcionado no es válido')
+      }
+
+      const result = await db.collection(collection).updateOne(
+        {_id: new ObjectId(id)},
+        { $set: newData}
+      )
+
+      if (result.matchedCount === 0) {
+        console.log('Paso 1:',result)
+
+        throw Boom.notFound(`No se encontró un documento con ID ${id} en la colección ${collection}`);
+      }
+
+      return result
+
+    } catch (error) {
+      if(Boom.isBoom(error)){
+        throw error
+      }else{
+        throw Boom.badImplementation('Sucito un error al actualizar la información',error)
+
+      }
+
+    }
+  }
+
+  async deleteOneById(collection,id){
+    try {
+      if(!ObjectId.isValid(id)){
+        throw Boom.badData('El ID proporcionado no es válido')
+      }
+
+      const result = await db.collection(collection).deleteOne({_id: new ObjectId(id)})
+
+      if(result.deletedCount === 0){
+        throw Boom.notFound('No encontrado')
+      }
+
+      return result
+
+    } catch (error) {
+      if(Boom.isBoom(error)){
+        throw error
+      }else{
+      throw Boom.badImplementation('Algo salio mal con la eliminación de este documento')
+      }
+    }
+  }
 
 }
+
+export default Collection
